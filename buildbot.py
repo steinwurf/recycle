2f59e54584e38efa3ced715ede296823f4f68a3c
Peter Vingelmann <petya@master.com>
2015-06-04 02:06:40 +0200

    Add buildbot.py

---
+++ buildbot.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+# encoding: utf-8
+
+project_name = 'recycle'
+
+def configure(options):
+    pass
+
+def build(options):
+    pass
+
+def run_tests(options):
+    pass
+
+def coverage_settings(options):
+    options['required_line_coverage'] = 50.0
